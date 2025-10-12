b90f03bf5c34dc973345f32f9bab66bb6469a748
Al3jandr0M4p <20230542@ipopsa.edu.do>
2025-10-12 17:09:54 -0600

    Creacion de estructura basica para navegacion del proyecto de practica

--- app/src/main/java/com/example/todoapp/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -10,8 +10,11 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.todoapp.ui.theme.TodoAppTheme
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.todoapp.screens.SplashScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,10 +23,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             TodoAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    MainNavigation(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +31,12 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun MainNavigation(modifier : Modifier) {
+    val navController = rememberNavController()
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TodoAppTheme {
-        Greeting("Android")
+    NavHost(navController = navController, startDestination = "splash") {
+        composable(route = "splash") {
+            SplashScreen()
+        }
     }
 }
---
+++ app/src/main/java/com/example/todoapp/screens/SpashScreen.kt
@@ -0,0 +1,11 @@
+package com.example.todoapp.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SplashScreen() {
+    Column {
+
+    }
+}
